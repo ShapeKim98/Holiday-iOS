81c9561b4ab779d465fe1099a7c10b8c82b48f8b
김도형 <shapekim98@gmail.com>
2025-02-16 18:31:38 +0900

    [feat] #3 테스트용 WeatherRepository 추가

--- Holiday/Domain/Repository/WeatherRepositoryProtocol.swift
+++ Holiday/Domain/Repository/WeatherRepositoryProtocol.swift
@@ -11,3 +11,15 @@ protocol WeatherRepositoryProtocol {
     func fetchWeather(id: Int) async throws -> WeatherEntity
     func fetchWeatherGroups(id: [Int]) async throws -> [WeatherEntity]
 }
+
+final class TestWeatherRepository: WeatherRepositoryProtocol {
+    func fetchWeather(id: Int) async throws -> WeatherEntity {
+        let response: WeatherResponse = .mockWeather
+        return response.list[0].toEntity(name: "서울", country: "대한민국")
+    }
+    
+    func fetchWeatherGroups(id: [Int]) async throws -> [WeatherEntity] {
+        let response = WeatherResponse.mockWeather
+        return []
+    }
+}
