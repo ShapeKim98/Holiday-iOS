5b694a49ce192f7b26095c6a6d7281a54f4d6680
김도형 <shapekim98@gmail.com>
2025-02-16 18:58:29 +0900

    [refactor] #3 네트워크 로깅 코드 분리

--- Holiday/Data/Network/NetworkProvider.swift
+++ Holiday/Data/Network/NetworkProvider.swift
@@ -12,26 +12,7 @@ import Alamofire
 struct NetworkProvider<E: URLRequestConvertible & EndPoint>: Sendable {
     func request<T: Decodable>(_ endPoint: E) async throws -> T {
 #if DEBUG
-        let request = try endPoint.asURLRequest()
-        print("[ℹ️] NETWORK -> request:")
-        print("""
-        method: \(request.httpMethod ?? ""),
-        url: \(request.url?.absoluteString ?? ""),
-        """
-        )
-        print("headers: ", terminator: "")
-        print("[")
-        for header in request.allHTTPHeaderFields ?? [:] {
-            print("  \(header.key): \(header.value),")
-        }
-        print("],\n")
-        if let body = request.httpBody {
-            let data: Data = try JSONSerialization.data(
-                withJSONObject: body,
-                options: [.prettyPrinted]
-            )
-            print("body: \(String(data: data, encoding: .utf8) ?? "nil")")
-        }
+        try NetworkLogger.request(endPoint)
 #endif
         let response = await AF.request(endPoint)
         .validate(statusCode: 200..<300)
@@ -39,26 +20,7 @@ struct NetworkProvider<E: URLRequestConvertible & EndPoint>: Sendable {
         .response
         
 #if DEBUG
-        print("[ℹ️] NETWORK -> response:")
-        if let urlResponse = response.response {
-            print("url: \(urlResponse.url?.absoluteString ?? "N/A"),")
-            print("status code: \(urlResponse.statusCode),")
-            print("headers: ", terminator: "")
-            let headers = urlResponse.allHeaderFields as? [String: String]
-            print("[")
-            for header in headers ?? [:] {
-                print("  \(header.key): \(header.value),")
-            }
-            print("],")
-        } else {
-            print(String(describing: response.response))
-        }
-        if let data = response.data {
-            let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
-            let jsonData = try JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted)
-            
-            print("body: \(String(data: jsonData, encoding: .utf8) ?? "nil")")
-        }
+        try NetworkLogger.response(response)
 #endif
         
         switch response.result {
