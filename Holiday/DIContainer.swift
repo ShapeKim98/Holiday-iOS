a0ebdd3683da69f15f3e0fa20cd5c8226dfd004a
김도형 <shapekim98@gmail.com>
2025-02-17 19:38:51 +0900

    [feat] #8 DIContainer 추가

---
+++ Holiday/DIContainer.swift
@@ -0,0 +1,30 @@
+//
+//  DIContainer.swift
+//  Holiday
+//
+//  Created by 김도형 on 2/17/25.
+//
+
+import Foundation
+
+final class DIContainer {
+    let shared = DIContainer()
+    
+    private init() { }
+    
+    private let photoRepository = PhotoRepository()
+    private let weatherRepository = WeatherRepository()
+    
+    func makeCityUseCase() -> CityUseCase {
+        return CityUseCase(
+            weatherRepository: self.weatherRepository,
+            photoRepository: self.photoRepository
+        )
+    }
+    
+    func makeCityListUseCase() -> CityListUseCase {
+        return CityListUseCase(
+            weatherRepository: self.weatherRepository
+        )
+    }
+}
