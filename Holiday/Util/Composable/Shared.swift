4d2aac5dc96984311277bd3a56a58e11ef0df6d5
김도형 <shapekim98@gmail.com>
2025-03-02 12:10:55 +0900

    [feat] #12 shared 프로퍼티래퍼 구현

---
+++ Holiday/Util/Composable/Shared.swift
@@ -0,0 +1,61 @@
+//
+//  UserDefault.swift
+//  EmotionDiary
+//
+//  Created by 김도형 on 12/31/24.
+//
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+@propertyWrapper
+struct Shared<T> {
+    private let type: SharedType<T>
+    
+    init(wrappedValue: T, _ type: SharedType<T>) {
+        self.type = type
+        self.wrappedValue = wrappedValue
+        
+        switch type {
+        case .userDefaults(let key):
+            if UserDefaults.standard.object(forKey: key.rawValue) == nil {
+                UserDefaults.standard.set(wrappedValue, forKey: key.rawValue)
+            }
+        }
+        
+    }
+    
+    var wrappedValue: T? {
+        get {
+            switch type {
+            case let .userDefaults(key):
+                return UserDefaults.standard.object(forKey: key.rawValue) as? T
+            }
+        }
+        set {
+            switch type {
+            case let .userDefaults(key):
+                UserDefaults.standard.set(newValue, forKey: key.rawValue)
+            }
+        }
+    }
+    
+    var projectedValue: Observable<T?> {
+        switch type {
+        case .userDefaults(let key):
+            UserDefaults.standard.rx
+                .observe(T.self, key.rawValue, options: [.initial, .new])
+                .share()
+        }
+    }
+}
+
+enum SharedType<T> {
+    case userDefaults(UserDefaultKey)
+}
+
+enum UserDefaultKey: String {
+    case cityId
+}
